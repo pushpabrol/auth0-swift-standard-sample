3ee04c63c65e1d2da54e14dbfdd7db7ac963c005
Pushp Abrol <pushpabrol@nd4l9r5x9k.lan>
2022-05-06 11:34:41 -0400

    first commit

---
+++ Sources/MainView.swift
@@ -0,0 +1,85 @@
+import SwiftUI
+import Auth0
+import SimpleKeychain
+
+struct MainView: View {
+    @State var profile = Profile.empty
+    @State var loggedIn = false
+    @State var credentialsManager = CredentialsManager(authentication: Auth0.authentication())
+    @State var rtAvailable = false
+    
+    
+    var body: some View {
+        if loggedIn {
+            VStack {
+                ProfileView(profile: self.$profile)
+                Button("Logout", action: self.logout)
+            }
+        } else {
+            VStack {
+                HeroView()
+                Button("Login", action: self.login)
+            }.onAppear {
+
+                
+                if (credentialsManager.hasValid() == false && A0SimpleKeychain().hasValue(forKey: "rtExists"))
+                {
+                    credentialsManager.enableBiometrics(withTitle: "Touch or enter passcode to Login")
+                }
+                
+                credentialsManager.credentials { result in
+                    switch result {
+                    case .success(let credentials):
+                        self.profile = Profile.from(credentials.idToken)
+                        A0SimpleKeychain().setString("Y", forKey: "rtExists")
+                        loggedIn = true
+                    case .failure(let error):
+                        loggedIn = false
+                        print("Failed with: \(error)")
+                    }
+                }
+                
+
+            }
+        }
+    }
+    
+}
+
+extension MainView {
+    
+     func login() {
+
+        Auth0
+            .webAuth()
+            .audience("organise")
+            .scope("openid profile email offline_access read:calendar")
+            .start { result in
+                switch result {
+                case .success(let credentials):
+                    self.loggedIn = credentialsManager.store(credentials: credentials)
+                    self.profile = Profile.from(credentials.idToken)
+                    if(credentials.refreshToken != nil) {rtAvailable = true}
+                case .failure(let error):
+                    print("Failed with: \(error)")
+                }
+            }
+    }
+
+    func logout() {
+        A0SimpleKeychain().clearAll()
+        _ = credentialsManager.clear()
+        Auth0
+            .webAuth()
+            .clearSession { result in
+                switch result {
+                case .success:
+                    
+                    self.loggedIn = false
+                case .failure(let error):
+                    print("Failed with: \(error)")
+                }
+            }
+    }
+    
+}
